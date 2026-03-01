fa88bf559f7b3dff77ab32013afdeacd55da0024
Gustavo Schmid de Jesus <gutors.nomad@gmai.com>
2026-03-01 15:19:26 -0300

    Add profiles dev and !dev
    -Set the profile using spring.profiles.active=profilename

--- src/main/java/com/gutors/lil/fid/ApplicationConfig.java
+++ src/main/java/com/gutors/lil/fid/ApplicationConfig.java
@@ -2,6 +2,7 @@ package com.gutors.lil.fid;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Profile;
 import org.springframework.context.annotation.PropertySource;
 
 import com.gutors.lil.fid.service.GreetingService;
@@ -18,8 +19,15 @@ public class ApplicationConfig {
     private String name;
 
     @Bean
+    @Profile("!dev")
     public TimeService timeService() {
         return new TimeService(true);
+    }
+
+    @Bean
+    @Profile("dev")
+    public TimeService devTimeService() {
+        return new TimeService(false);
     }
 
     @Bean
