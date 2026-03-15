1997e15fe62419819fc3c023060eabc6ef3c2483
Gustavo Schmid de Jesus <gutors.nomad@gmai.com>
2026-03-15 15:58:52 -0300

    Add @AfterReturning aspect

--- src/main/java/com/gutors/lil/fid/aspect/LoggingAspect.java
+++ src/main/java/com/gutors/lil/fid/aspect/LoggingAspect.java
@@ -1,8 +1,10 @@
 package com.gutors.lil.fid.aspect;
 
 import java.util.Arrays;
+import java.util.Collection;
 
 import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.AfterReturning;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.aspectj.lang.annotation.Pointcut;
@@ -37,6 +39,31 @@ public class LoggingAspect {
             sb.append("]");
         }
         
+        LOGGER.info(sb.toString().replace("| ]", "]"));
+    }
+
+    // execute this after the main @Pointcut method and log the return value
+    @AfterReturning(pointcut = "executeLogging()", returning = "returnValue")
+    public void logMethodExecution(JoinPoint joinPoint, Object returnValue) {
+        StringBuilder sb = new StringBuilder("Method=");
+        sb.append(joinPoint.getSignature().getName());
+       
+        Object[] args = joinPoint.getArgs();
+        
+        if (args != null && args.length > 0) {
+            sb.append(" | args=[ ");
+            Arrays.asList(args).forEach(arg -> {
+                sb.append(arg).append(" | ");
+            });
+            sb.append("]");
+        }
+
+        if (returnValue instanceof Collection) {
+            sb.append(" returning: ").append(((Collection)returnValue).size()).append("instances");
+        } else {
+            sb.append(" returning: ").append(returnValue.toString());
+        }
+
         LOGGER.info(sb.toString().replace("| ]", "]"));
     }
 
